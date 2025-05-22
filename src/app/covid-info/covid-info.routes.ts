8e10abc52a64f91b2ed772924dcadbc0448cbcf2
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-05-22 09:29:08 +0200

    add routing for covid-info module with HomeComponent

--- src/app/covid-info/covid-info.routes.ts
+++ src/app/covid-info/covid-info.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-export const covidInfoRoutes: Routes = [];
+export const covidInfoRoutes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    }
+];
