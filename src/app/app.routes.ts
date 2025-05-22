a9d27c4e758dd40727ba9d17824c50ce3150f2a3
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-05-22 09:28:06 +0200

    fix routing to redirect from root to covid-info path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+    {path: '', redirectTo: 'covid-info', pathMatch: 'full'},    
     {
-        path: '',
+        path: 'covid-info',
         loadChildren: () => import('./covid-info/covid-info.routes').then(m => m.covidInfoRoutes),
     }
 ];
