7d914109ddfeeda5a40b3054fde1621e6b994fef
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-05-22 09:27:04 +0200

    add routing for covid-info module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadChildren: () => import('./covid-info/covid-info.routes').then(m => m.covidInfoRoutes),
+    }
+];
