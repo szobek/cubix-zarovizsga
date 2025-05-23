dac2e8c479886b686abd4ed345299348dfb89eba
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-05-23 19:40:15 +0200

    refactor AuthService: streamline user registration and login methods, improve code readability

--- src/app/auth/components/auth.service.ts
+++ src/app/auth/components/auth.service.ts
@@ -6,18 +6,22 @@ import { User } from '../models/User.model';
   providedIn: 'root'
 })
 export class AuthService {
-  private readonly _registeredUsersKey = 'registeredUsers';
-  user:User|null = null;
-
+  private readonly _registeredUsersKey='registeredUsers';
+  private readonly _currentUserKey='currentUser';
+  user:User|null=null;
   router:Router=inject(Router);
-  register(email: string, password: string): void {
-    const registeredUsers = JSON.parse(localStorage.getItem(this._registeredUsersKey) || '[]');
-    const userExists = registeredUsers.some((user: { email: string }) => user.email === email);
+  
+  constructor() {
+    this.loadUser()
+  }
 
+  register(email: string, password: string): void {
+    const registeredUsers=JSON.parse(localStorage.getItem(this._registeredUsersKey) || '[]');
+    const userExists=registeredUsers.some((user: { email: string }) => user.email === email);
     if (userExists) {
       alert('User already exists');
     } else {
-      const newUser:User = {
+      const newUser:User={
         id: this.uuidv4(),
         name: '',
         email,
@@ -26,17 +30,17 @@ export class AuthService {
       registeredUsers.push(newUser);
       localStorage.setItem(this._registeredUsersKey, JSON.stringify(registeredUsers));
       this.router.navigate(['/login']);
-
     }
   }
 
   login(email: string, password: string): void {
-    const registeredUsers = JSON.parse(localStorage.getItem(this._registeredUsersKey) || '[]');
-    const user = registeredUsers.find((user: { email: string; password: string }) => user.email === email && user.password === password);
+    const registeredUsers=JSON.parse(localStorage.getItem(this._registeredUsersKey) || '[]');
+    const user=registeredUsers.find((user: { email: string; password: string }) => user.email === email && user.password === password);
 
     if (user) {
       this.router.navigate(['/covid-info']);
-      this.user = user;
+      this.user=user;
+      localStorage.setItem(this._currentUserKey, JSON.stringify(user));
 
     } else {
       alert('Invalid email or password');
@@ -44,18 +48,16 @@ export class AuthService {
   }
 
   logout(): void {
-    this.user = null;
+    this.user=null;
     this.router.navigate(['/login']);
   }
 
-  loadUser(): void {
-    const registeredUsers = JSON.parse(localStorage.getItem(this._registeredUsersKey) || '[]');
-    const user = registeredUsers.find((user: { email: string }) => user.email === this.user?.email);
-
+  private loadUser(): void {
+    const user=localStorage.getItem(this._currentUserKey);
     if (user) {
-      this.user = user;
+      this.user=JSON.parse(user);
     } else {
-      this.user = null;
+      this.user=null;
     }
   }
 
