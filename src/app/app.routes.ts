50ce6add711979e04456e622bfa48903f5a51116
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-05-23 15:47:19 +0200

    add login component with form validation and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'covid-info', pathMatch: 'full'},    
+    {path: 'login', loadComponent: () => import('./auth/components/login/login.component').then(m => m.LoginComponent)},
     {path:'registration', loadComponent: () => import('./auth/components/registration/registration.component').then(m => m.RegistrationComponent)},
     {
         path: 'covid-info',
--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'czv-login',
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm:FormGroup = new FormGroup({
+    email: new FormControl('',[Validators.required, Validators.email]),
+    password: new FormControl('',[Validators.required, Validators.minLength(6)]),
+  });
+
+  onSubmit() {
+    console.log(this.loginForm.value);
+  }
 
 }
