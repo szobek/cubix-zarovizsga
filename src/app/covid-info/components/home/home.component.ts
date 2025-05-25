0ddd1d53a52a6b2346b553ed5e45563652ef10fa
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-05-25 14:45:07 +0200

    refactor: restructure HomeComponent state management and update template for improved data handling

--- src/app/covid-info/components/home/home.component.ts
+++ src/app/covid-info/components/home/home.component.ts
@@ -5,11 +5,27 @@ import { finalize, tap } from 'rxjs/operators';
 import { CovidData } from '../../models/CovidData.model';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import { AuthService } from '../../../auth/auth.service';
+import {MatTableModule} from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import { NgForOf } from '@angular/common';
+
+interface HomeState {
+  countries: string[];
+  loader: boolean;
+  dataSource: CovidData[];
+  displayedColumns: string[];
+  selectedCountry: string;
+}
 @Component({
   selector: 'czv-home',
   imports: [
     FormsModule,
     MatProgressSpinnerModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    NgForOf
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
@@ -17,32 +33,29 @@ import { AuthService } from '../../../auth/auth.service';
 export class HomeComponent {
   covidCallService: CovidCallService=inject(CovidCallService);
   authService:AuthService=inject(AuthService);
-  countries: string[]=['Hungary', 'Slovakia', 'Slovenia', 'France', 'Austria','Romania'];
-  selectedCountry: string='';
-  loader: boolean = false;
+
+  state:HomeState={
+    "countries": ['Hungary', 'Slovakia', 'Slovenia', 'France', 'Austria','Romania'],
+    "loader": false,
+    "dataSource": [] ,
+    "displayedColumns": ['country', 'confirmed','deaths'],
+    "selectedCountry":''
+  }
   onCountryChange($event: any) {
     const country=$event;
-    if(country===""){
-      return
-    }
-    this.loader = true;
+    if(country===""){return}
+    this.state.loader=true;
     this.covidCallService.getCovidData(country)
     .pipe(
       tap((data:CovidData)=>{
         this.authService.increaseUsedAPI()
         if(localStorage.getItem('usedAPI')===null){}
-        document.title = `Covid data for ${country}`;
-        document.querySelector('.country')!.textContent=country;
-        document.querySelector('.deaths')!.textContent=data.deaths.toString();
-        document.querySelector('.confirmed')!.textContent=data.confirmed.toString();
+        this.state.dataSource=[data]; 
         }),
         finalize(() => {
-          this.loader=false;
+          this.state.loader=false;
         })
     )
     .subscribe()
-  
   }
-
-
 }
